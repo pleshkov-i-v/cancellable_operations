9d1a4e65dbb437e9d87b3f455763f855d3307ddd
Ivan Pleshkov <pleshkov.i.v@gmail.com>
2026-02-11 16:46:54 +0300

    wrong

---
+++ lib/home/home_page.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Future? _operation;
+
+  void _startOperation() {
+    _operation = _operationFunction();
+  }
+
+  void _stopOperation() {
+    _operation = null;
+  }
+
+  Future<void> _operationFunction() async {
+    for (int i = 0; i < 10; i++) {
+      await Future.delayed(const Duration(seconds: 1));
+      setState(() {
+        _counter = i;
+      });
+    }
+    setState(() {
+      _operation = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              _operation != null
+                  ? 'Operation in progress'
+                  : 'There is no operation in progress',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _startOperation,
+            tooltip: 'Start',
+            child: const Text('Start'),
+          ),
+          const SizedBox(width: 20),
+          FloatingActionButton(
+            onPressed: _stopOperation,
+            tooltip: 'Stop',
+            child: const Text('Stop'),
+          ),
+        ],
+      ),
+    );
+  }
+}
